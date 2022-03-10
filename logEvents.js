3c6d6cf156899721e6532c38893a58c8724428c4
Rohan Mishra <rohanmishra.004@gmail.com>
2022-03-10 13:48:37 +0530

    First commit

---
+++ logEvents.js
@@ -0,0 +1,32 @@
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+const fsPromises = require('fs').promises
+const fs = require('fs');
+const path = require('path');
+const { mainModule } = require('process');
+
+
+
+const logEvents = async(message, logName)=>{
+    const dateTime = `${format(new Date(), `yyyyMMdd\tHH:mm:ss`)}`;
+    const logItem = `\n${dateTime} \t${uuid()} \t${message}\n`;
+    console.log(logItem);
+
+    try{
+        if (!fs.existsSync(path.join(__dirname, 'logs'))){
+            await fsPromises.mkdir(path.join(__dirname,'logs'))
+        }
+        //testing
+        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem)
+
+    }catch(err){
+        console.log(err)
+    }
+
+}
+
+
+module.exports = logEvents;
+
+
+
